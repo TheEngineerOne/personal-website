310e450987f9ad724f7c176cba86717d420aaa8c
theengineerone <verbruggenlucas2@gmail.com>
2025-05-20 14:42:25 +0200

    Adding basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@
 import './App.css'
 import {TopBar} from './Components/SimpleTopBar'
 import {FullScreenComponent} from './Components/FullScreenComponent'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import NotFound from './pages/NotFound.tsx'
+import About from './pages/About.tsx'
 
 function App() {
   return (
     <>
+      <Router>
       <TopBar/>
       <FullScreenComponent>
+        <Routes>
+          <Route path="/about" element={<About/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
       </FullScreenComponent>
+      </Router>
     </>
   )
 }
