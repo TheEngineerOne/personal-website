7802a658cf5b6f99e5d1b7b4f0a1927c39fe85ba
theengineerone <verbruggenlucas2@gmail.com>
2025-05-20 13:58:58 +0200

    Removing bad paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@
 import './App.css'
 import {TopBar} from './Components/SimpleTopBar'
 import {FullScreenComponent} from './Components/FullScreenComponent'
-import {Paragraph} from './Components/Paragraph'
 
 function App() {
   return (
     <>
       <TopBar/>
       <FullScreenComponent>
-        <Paragraph title="How did I start programming" text=""/>
       </FullScreenComponent>
     </>
   )
