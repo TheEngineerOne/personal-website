33044809ce89c997069e2886eb7be1e3c73c0475
theengineerone <verbruggenlucas2@gmail.com>
2025-05-21 23:14:12 +0200

    Adding correctly for language and tech in about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,6 +6,55 @@ const About : React.FC = () => {
       <p>
         Je suis VERBRUGGEN Lucas, un développeur backend, frontend et fullstack
       </p>
+      <h1>Techonologies maitr&icirc;s&eacute;es</h1>
+      <div className="items-center">
+        <div className="flex items-center overflow-auto space-x-5">
+          <div className="flex flex-col items-center flex-shrink-0">
+              <img className="w-auto h-16" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Typescript.svg/1024px-Typescript.svg.png"/>
+              <span>TypeScript</span>
+          </div>
+          <div className="flex flex-col items-center flex-shrink-0">
+              <img className="w-auto h-16" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/250px-Unofficial_JavaScript_logo_2.svg.png"/>
+              <span>JavaScript</span>
+          </div>
+          <div className="flex flex-col items-center flex-shrink-0">
+              <img className="w-auto h-16" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/115px-Python-logo-notext.svg.png"/>
+              <span>Python</span>
+          </div>
+          <div className="flex flex-col items-center flex-shrink-0">
+              <img className="w-auto h-16" src="https://upload.wikimedia.org/wikipedia/fr/thumb/2/2e/Java_Logo.svg/250px-Java_Logo.svg.png"/>
+              <span>Java</span>
+          </div>
+          <div className="flex flex-col items-center flex-shrink-0">
+              <img className="w-auto h-16" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/330px-PHP-logo.svg.png"/>
+              <span>PHP</span>
+          </div>
+          <div className="flex flex-col items-center flex-shrink-0">
+              <img className="w-auto h-16" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Kotlin_Icon.svg/250px-Kotlin_Icon.svg.png"/>
+              <span>Kotlin</span>
+          </div>
+          <div className="flex flex-col items-center flex-shrink-0">
+              <img className="w-auto h-16" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/250px-ISO_C%2B%2B_Logo.svg.png"/>
+              <span>C++</span>
+          </div>
+          <div className="flex flex-col items-center flex-shrink-0">
+              <img className="w-auto h-16" src="https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png"/>
+              <span>C</span>
+          </div>
+          <div className="flex flex-col items-center flex-shrink-0">
+            <img className="w-auto h-16" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/50px-Laravel.svg.png"/>
+            <span>Laravel</span>
+          </div>
+          <div className="flex flex-col items-center flex-shrink-0">
+            <img className="w-auto h-16" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/330px-React-icon.svg.png"/>
+            <span>ReactJS</span>
+          </div>
+          <div className="flex flex-col items-center flex-shrink-0">
+              <img className="w-auto h-16" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Godot_icon.svg/250px-Godot_icon.svg.png"/>
+              <span>Godot Engine</span>
+          </div>
+        </div>
+      </div>
       <h1>Mes comp&eacute;tences</h1>
       <p>
         <span>Je maitr&icirc;se les langages suivants :</span>
@@ -59,6 +108,7 @@ const About : React.FC = () => {
             </div>
           </li>
         </ul>
+
       </p>
       <p>
         <span>Je maitr&icirc;se les frameworks suivants :</span>
@@ -73,6 +123,12 @@ const About : React.FC = () => {
             <div className="flex space-x-2">
               <img className="w-auto h-5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/330px-React-icon.svg.png"/>
               <span>ReactJS</span>
+            </div>
+          </li>
+          <li>
+            <div className="flex space-x-2">
+              <img className="w-auto h-5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Godot_icon.svg/250px-Godot_icon.svg.png"/>
+              <span>Godot Engine</span>
             </div>
           </li>
         </ul>
