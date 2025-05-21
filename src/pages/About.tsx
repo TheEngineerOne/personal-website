d05b0ffbc3cdec7691f580fd7ed09de7ac4cd747
theengineerone <verbruggenlucas2@gmail.com>
2025-05-21 10:18:11 +0200

    Removing all mentions of localisations

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import FullScreenComponent from '../Components/FullScreenComponent'
-import en_loc from "../assets/locales/en/about.json"
 const About : React.FC = () => {
   return (
     <>
       <FullScreenComponent>
-        <h1>{en_loc['About Me']}</h1>
-        <p>{en_loc['Presentation']}</p>
       </FullScreenComponent>
     </>
   )
