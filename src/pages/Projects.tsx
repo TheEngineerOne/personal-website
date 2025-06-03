466d572aaf8103566e83d50e21f8b71c316c7f1f
theengineerone <verbruggenlucas2@gmail.com>
2025-06-03 11:00:42 +0200

    adding testing case for the website

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import ProjectCard from "../Components/ProjectCard.tsx";
 
 const Projects : React.FC = () => {
   return (
     <>
-        <h1>Test Projects</h1>
+        <ProjectCard
+        href=""
+        altText="Lorem Ipsum"
+        imageUrl="https://picsum.photos/id/237/200/300"
+        text="Lorem Ipsum"
+        />
     </>
   )
 }
