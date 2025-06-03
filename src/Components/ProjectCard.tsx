f8d1c45ee22357156a95c462902a56afb7048a47
theengineerone <verbruggenlucas2@gmail.com>
2025-06-03 13:45:19 +0200

    Changing scaling for ProjectCard

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -15,11 +15,11 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
 }) => {
   return (
     <div className='flex flex-col items-center gap-2 w-full p-2'>
-      <a href={href} target="_blank" rel="noopener noreferrer" className="w-full">
+      <a href={href} target="_blank" rel="noopener noreferrer" className="w-full transiion-transform hover:scale-105">
         <img 
           src={imageUrl} 
           alt={altText} 
-          className="w-full mx-auto center rounded shadow-md transition-transform hover:scale-105"
+          className="w-full mx-auto center rounded shadow-md"
         />
         <div className="mx-auto text-center text-xl">
           {text}
