077221053118bc6961ca246be0c73f11b730498c
theengineerone <verbruggenlucas2@gmail.com>
2025-06-04 11:44:12 +0200

    Adding lisn experience to experiences

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import ProjectCard from "../Components/ProjectCard.tsx"
 
 const Experience : React.FC = () => {
   return (
     <>
-        <h1>Test Experience</h1>
+      <div>
+        <ProjectCard
+          href="https://www.lisn.upsaclay.fr/"
+          imageUrl="https://www.lisn.upsaclay.fr/app/uploads/2022/01/logo.svg"
+          text="Stagiaire Développeur au LISN (Janvier 2025 - Mai 2025)"
+        />
+      </div> 
     </>
   )
 }
