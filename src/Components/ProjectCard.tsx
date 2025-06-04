6a69736ec9fed6b0b818bfb38d6fbae9037859b3
theengineerone <verbruggenlucas2@gmail.com>
2025-06-04 11:50:12 +0200

    Making ProjectCard rounded

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -14,17 +14,19 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   text
 }) => {
   return (
-    <div className='flex flex-col items-center gap-2 w-full p-5'>
-      <a href={href} target="_blank" rel="noopener noreferrer" className="w-full transiion-transform hover:scale-105">
-        <img 
-          src={imageUrl} 
-          alt={altText} 
-          className="w-full mx-auto center rounded shadow-md"
-        />
-        <div className="mx-auto text-center text-xl">
-          {text}
-        </div>
-      </a>
+    <div className="p-5">
+      <div className='flex bg-neutral-500 flex-col rounded items-center gap-2 w-full p-5'>
+        <a href={href} target="_blank" rel="noopener noreferrer" className="w-full transiion-transform hover:scale-105">
+          <img 
+            src={imageUrl} 
+            alt={altText} 
+            className="w-full mx-auto center rounded shadow-md"
+          />
+          <div className="mx-auto text-center text-xl">
+            {text}
+          </div>
+        </a>
+      </div>
     </div>
   );
 };
